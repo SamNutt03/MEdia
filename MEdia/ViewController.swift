2e47d939f6259d7774c8a229396e976ac152d31d
SamNutt03 <nuttallsam@yahoo.co.uk>
2025-06-11 13:45:41 +0100

    Initial Commit

---
+++ MEdia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MEdia
+//
+//  Created by Sam Nuttall on 11/06/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MEdiaTests/MEdiaTests.swift
@@ -0,0 +1,17 @@
+//
+//  MEdiaTests.swift
+//  MEdiaTests
+//
+//  Created by Sam Nuttall on 11/06/2025.
+//
+
+import Testing
+@testable import MEdia
+
+struct MEdiaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
