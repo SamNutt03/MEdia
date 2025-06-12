8b1ec285225604bbd44ddb70ba126ed526a99a8c
SamNutt03 <nuttallsam@yahoo.co.uk>
2025-06-12 01:35:02 +0100

    Added more assets. Changed and relocated button on profile customisation view. changed blur levels to improve UI of customise view.

--- MEdia/CustomiseViewController.swift
+++ MEdia/CustomiseViewController.swift
@@ -12,11 +12,13 @@ class CustomiseViewController: UIViewController {
     @IBAction func buttonn(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let blurView = UIVisualEffectView(effect: UIBlurEffect(style: .light))
         blurView.frame = view.bounds
+        blurView.alpha = 0.95
         blurView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         view.insertSubview(blurView, at: 0)
         
